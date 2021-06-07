2d0f72f0e89b4015000592ffcb3be2ac9eae8c67
NamMinHyuk <skaalsgur156@naver.com>
2021-06-07 16:33:46 +0900

    algolrithm

--- algorithm01/src/algorithm/algorithm01.java
+++ algorithm01/src/algorithm/algorithm01.java
@@ -1,5 +1,7 @@
 package algorithm;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class algorithm01 {
@@ -12,47 +14,66 @@ public class algorithm01 {
 		String hand = scanner.nextLine();
 		numbers = numbers.replace(" ", "");
 		int[] number = new int[numbers.length()];
-		for(int i=0;i<number.length;i++) {
+		for (int i = 0; i < number.length; i++) {
 			number[i] = Character.getNumericValue(numbers.charAt(i));
 		}
-		System.out.println(solution(number,hand));
+		System.out.println(solution(number, hand));
 		scanner.close();
 	}
+
 	public static String solution(int[] numbers, String hand) {
-		String[][] keypad = {{"1","2","3"},{"4","5","6"},{"7","8","9"},{"*","0","#"}};
-		String left = "1";
-		String right = "3";
-		for(int i=0;i<keypad.length;i++) {
-			for(int j=0;j<keypad[i].length;j++) {
-				//System.out.printf(keypad[i][j] + " , ");
+		String answer = "";
+		// 키패드, 좌표 설정
+		Map<String, String> keypad = new HashMap<String, String>();
+		int count = 1;
+		for(int i=1;i<4;i++) {
+			for(int j=1;j<4;j++) {
+				keypad.put(Integer.toString(count),Integer.toString(i)+","+Integer.toString(j));
+				count++;
 			}
 		}
-		String answer = "";
-		for(int i=0;i<numbers.length;i++) {
-			if(numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7) {
+		keypad.put("*","4,1");
+		keypad.put("0", "4,2");
+		keypad.put("#", "4,3");
+		// 왼손 좌표 초기화
+		String left = "*";
+		String left_loc = keypad.get(left);	
+		
+		// 오른손 좌표 초기화
+		String right = "#";
+		String right_loc = keypad.get(right);		
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7) {
 				answer += "L";
 				left = Integer.toString(numbers[i]);
-			}else if(numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9) {
+			} else if (numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9) {
 				answer += "R";
 				right = Integer.toString(numbers[i]);
-			}else if(numbers[i] == 2 || numbers[i] == 5 || numbers[i] == 8 || numbers[i] == 0) {
-				if((numbers[i] - Integer.parseInt(left)) > (numbers[i] - Integer.parseInt(right))) {
-					answer += "R";
-					right = Integer.toString(numbers[i]);
-				}else if((numbers[i] - Integer.parseInt(left)) == (numbers[i] - Integer.parseInt(right))) {
-					if(hand == "right") {
-						answer += "R";
-						right = Integer.toString(numbers[i]);
-					}else {
+			} else if (numbers[i] == 2 || numbers[i] == 5 || numbers[i] == 8 || numbers[i] == 0) {
+					
+					int left_res = (int)(Math.abs(Double.parseDouble(keypad.get(left).substring(0, 1)) - Double.parseDouble(keypad.get(Integer.toString(numbers[i])).substring(0, 1))) +
+					Math.abs(Double.parseDouble(keypad.get(left).substring(2, 3)) - Double.parseDouble(keypad.get(Integer.toString(numbers[i])).substring(2, 3))));
+					
+					int right_res = (int)(Math.abs(Double.parseDouble(keypad.get(right).substring(0, 1)) - Double.parseDouble(keypad.get(Integer.toString(numbers[i])).substring(0, 1))) +
+					Math.abs(Double.parseDouble(keypad.get(right).substring(2, 3)) - Double.parseDouble(keypad.get(Integer.toString(numbers[i])).substring(2, 3))));
+					
+					if(left_res < right_res) {
 						answer += "L";
 						left = Integer.toString(numbers[i]);
+					}else if(left_res == right_res) {
+						if(hand.equals("right")) {
+							answer += "R";
+							right = Integer.toString(numbers[i]);
+						}else {
+							answer += "L";
+							left = Integer.toString(numbers[i]);
+						}
+					}else {
+						answer += "R";
+						right = Integer.toString(numbers[i]);
 					}
-				}else {
-					answer += "L";
-					left = Integer.toString(numbers[i]);
-				}
 			}
-		}
+		} 
 		return answer;
 	}
 }
